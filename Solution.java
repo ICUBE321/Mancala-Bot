5d7d34d1d9379cbc2e58ac9d88c80ca25cced1fe
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2020-02-06 15:07:01 -0500

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.Scanner;
 
 /*
-1
+2
 32
 1 1 3 1 0 0 
 9
@@ -21,27 +21,36 @@ public class Solution {
     public static void printNextMove(int player, int player1Mancala, int player1Marbles[], int player2Mancala, int player2Marbles[]){
         
         int hole, index;
-        ArrayList<Integer> holes = new ArrayList<Integer>();
-        ArrayList<Integer> scores = new ArrayList<Integer>();
+        ArrayList<Integer> holes = new ArrayList<Integer>();//list to store potential hole numbers
+        ArrayList<Integer> scores = new ArrayList<Integer>();//list to store the possibles scores of the potential hole numbers
         
-        //case free turn
-        if(player1Marbles[5] == 1) {
-            hole = 6;
-            System.out.print(hole);
-            return;
+        
+        hole = 1; //hole number
+        index = hole - 1; //corresponding array index
+        //case win and case manc
+        while(index < 6) {
+            if(case_win(player1Marbles, index, hole)) {//checks if you land a mancala and get a free turn
+            	System.out.println(hole);//select hole
+            	return;//exit function
+            } else if(case_manc(player1Marbles, index, hole)) {//checks if you can land past mancala
+                holes.add(hole);//holding the value for the hole number for prev case
+                scores.add(player1Mancala + 1);//holding the potential score if the hole is picked
+            } 
+                hole++;//updates hole number to the next hole
+                index++;//updates index number to the next index
         }
         
-      //changing the starting hole for the next case
+        //changing the starting hole for the next case
         hole = 1;
         index = hole - 1;  
         //case 4
         while(index < 6) {
-            if(player2Marbles[index] == 13 && !check_empty(player1Marbles, hole-1)) {
-                System.out.println(hole);
-                return;
+            if(player2Marbles[index] == 13 && !check_empty(player1Marbles, hole-1)) {//checks if the opponent has 13 marbles and your corresponding hole is not empty
+                System.out.println(hole);//select hole
+                return;//exit function
             } 
-                hole++;
-                index++;
+                hole++;//updates hole number to the next hole
+                index++;//updates index number to the next index
         }
         
         //changing the starting hole for the next case
@@ -50,9 +59,9 @@ public class Solution {
         //case 1
         int target;//for holding the empty hole's hole number
         while(index < 6) {    
-            if(player1Marbles[index] == 0 && player2Marbles[5-index] > 0) {
-                target = hole;
-                //checking holes before the target
+            if(player1Marbles[index] == 0 && player2Marbles[5-index] > 0) {//checks if you have an empty hole and opponents corresponding hole is not empty
+                target = hole;//stores your empty hole's number as target
+                //checking holes before the target to see if any can land in the target
                 for(int i=index-1; i>=0; i--) {
                     //if a hole has enough seeds to land in the target
                     if(player1Marbles[i] == target-(i+1)) {
@@ -62,33 +71,8 @@ public class Solution {
                     }
                 }
             }
-                hole++;
-                index++;
-        }
-        
-        hole = 1; //hole number
-        index = hole - 1; //corresponding array index
-        //case win and case manc
-        while(index < 6) {
-            if(case_win(player1Marbles, index, hole)) {
-            	int count = 0;
-                for(int i=index+1; i<6;i++) {
-                	if(player1Marbles[i] != 0) {
-                		count++;
-                	}
-                }
-                if(count == 0) {
-                	System.out.println(hole);
-                	return;
-                }
-                holes.add(hole);//holding the value for the hole number for prev case
-                scores.add(player1Mancala + 1);//holding the potential score if hole is picked
-            } else if(case_manc(player1Marbles, index, hole)) {
-                holes.add(hole);//holding the value for the hole number for prev case
-                scores.add(player1Mancala + 1);//holding the potential score if hole is picked
-            } 
-                hole++;
-                index++;
+                hole++;//updates hole number to the next hole
+                index++;//updates index number to the next index
         }
         
         //changing the starting hole for the next case
@@ -96,24 +80,24 @@ public class Solution {
         index = hole - 1;  
         //case 3
         while(index < 6) {
-            if(player1Marbles[index] == 13) {
+            if(player1Marbles[index] == 13) {//checks if you have 13 marbles 
                 //System.out.println(hole);
                 //return;
                  holes.add(hole);//holding the value for the hole number for prev case
                  scores.add(player1Mancala + player1Marbles[hole - 1] + player2Marbles[6 - hole]);//holding the potential score if hole is picked
             } 
-                hole++;
-                index++;
+                hole++;//updates hole number to the next hole
+                index++;//updates index number to the next index
         }
         
         //case random 
-        hole = getRandomInteger(6,1);
+        hole = getRandomInteger(6,1);//selects a random integer from 1 to 6
         index = hole - 1;
-        while(player1Marbles[hole] == 0 || index > 5 || index < 0){
+        while(player1Marbles[hole] == 0 || index > 5 || index < 0){//check if the random hole is valid
             hole = getRandomInteger(6,1);
             index = hole - 1;
         }
-        if(case_manc(player1Marbles, index, hole)) {
+        if(case_manc(player1Marbles, index, hole)) {//checks if you can land past mancala
             holes.add(hole);//holding the value for the hole number for prev case
             scores.add(player1Mancala + 1);//holding the potential score if hole is picked
         } else {
@@ -121,10 +105,11 @@ public class Solution {
             scores.add(player1Mancala);//holding the potential score if hole is picked
         }
         
-        int max_index = scores.indexOf(Collections.max(scores));
-        System.out.print(holes.get(max_index));
+        int max_index = scores.indexOf(Collections.max(scores));//get the index of the max score
+        System.out.print(holes.get(max_index));//picks the hole of that max score
     }
     
+    //function to check if you land a mancala and get a free turn
     public static Boolean case_win(int playerMarbles[], int index, int hole) {
         Boolean ans = false;
         if(playerMarbles[index] == 7-hole) {
@@ -135,7 +120,7 @@ public class Solution {
     
     public static Boolean case_manc(int playerMarbles[], int index, int hole) {
         Boolean answ = false;
-        if(playerMarbles[index] >= 7-hole) {
+        if(playerMarbles[index] > 7-hole) {
             answ = true;
         }
         return answ;
